f78f29efbb99fa0978ac1b5b121079090ec74e9a
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-01-31 00:21:12 +0100

    homepage

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Homepage = () => {
     return (
         <div className='min-h-screen bg-black'>
-            
+            <img src='https://as1.ftcdn.net/v2/jpg/02/22/10/22/1000_F_222102223_rTf36xg0GGGCewL8YLZKZvJ85PIqID80.jpg' alt=''/>
         </div>
     );
 };
