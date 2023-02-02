3f2186b1b141221570878d68d0d0bbd6a243c9d8
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-02-02 11:36:50 +0100

    homepage

--- src/Pages/Homepage/ArticlesStack.js
+++ src/Pages/Homepage/ArticlesStack.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ArticlesStack = () => {
+    const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("Articles.json")
+      .then((res) => res.json())
+      .then((data) => setArticles(data));
+  }, []);
     return (
         <div>
-            
+            {
+                articles?.map(article=>
+                <p>{article.author}</p>
+                )
+            }
         </div>
     );
 };
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ArticlesStack from './ArticlesStack';
 import HomeBanner from './HomeBanner';
 
 const Homepage = () => {
+
     return (
         <div className='min-h-screen '>
             <HomeBanner/>
+            <ArticlesStack/>
+            
         </div>
     );
 };
