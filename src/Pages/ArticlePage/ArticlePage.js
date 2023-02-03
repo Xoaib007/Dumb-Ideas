d66bfc7e9c601726756936c8f70dee338398fd68
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-02-03 23:38:05 +0100

    router

--- src/Pages/ArticlePage/ArticlePage.js
+++ src/Pages/ArticlePage/ArticlePage.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Footer from '../../Components/Shared/Footer/Footer';
-import Navbar from '../../Components/Shared/Navbar/Navbar';
 import Articles from './Articles';
 
 const ArticlePage = () => {
     return (
         <div>
-            <Navbar/>
             <Articles/>
-            <Footer/>
         </div>
     );
 };
