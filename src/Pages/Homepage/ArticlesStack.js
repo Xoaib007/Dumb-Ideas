844d2271f07e76b3f12e14732155ad31716f2df9
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-02-03 12:33:08 +0100

    router

--- src/Pages/Homepage/ArticlesStack.js
+++ src/Pages/Homepage/ArticlesStack.js
@@ -4,21 +4,25 @@ import { Link } from 'react-router-dom';
 const ArticlesStack = () => {
     const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    fetch("Articles.json")
-      .then((res) => res.json())
-      .then((data) => setArticles(data));
-  }, []);
+    useEffect(() => {
+        fetch("Articles.json")
+            .then((res) => res.json())
+            .then((data) => setArticles(data));
+    }, []);
     return (
         <div>
             {
-                articles?.map((article, i)=>
-                <Link to={`/article/${article.id}`} key={i} className='border-8'>
-                    <p>{article.title}</p>
-                    <p>{article.author}</p>
-                    <p>{article.publishedDate}</p>
-                    <p>{article.shortDescription}</p>
-                </Link>
+                articles?.map((article, i) =>
+                    <div key={i} className='border-b-2 border-gray-800 mx-40 my-20'>
+                        <Link to={`/article/${article.id}`}>
+                            <p className='text-3xl font-bold'>{article.title}</p>
+                            <div className='flex'>
+                                <p>{article.author}</p>
+                                <p>{article.publishedDate}</p>
+                            </div>
+                            <p>{article.shortDescription}</p>
+                        </Link>
+                    </div>
                 )
             }
         </div>
