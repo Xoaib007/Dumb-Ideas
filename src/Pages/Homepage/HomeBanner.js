8fb79982f1946c0693196bca60e55630a4700688
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-02-04 20:38:57 +0100

    navbar

--- src/Pages/Homepage/HomeBanner.js
+++ src/Pages/Homepage/HomeBanner.js
@@ -6,7 +6,7 @@ const HomeBanner = () => {
     return (
         <div className='bgbanner h-[40rem]'>
         <Navbar/>
-            <p className='text-5xl font-bold text-white pt-60 pl-20'>No Ideas Are<br/><span className='text-8xl font-bold text-white'>DUMB...</span> <br/>Share your dumb thoughts</p>
+            <p className='text-5xl font-bold text-white pt-48 pl-20'>No Ideas Are<br/><span className='text-8xl font-bold text-white'>DUMB...</span> <br/>Share your dumb thoughts</p>
         </div>
     );
 };
