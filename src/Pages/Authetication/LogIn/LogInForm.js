891071c872c1febf29fafdc566f216aa2ff94de0
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-02-05 13:03:15 +0100

    navbar

--- src/Pages/Authetication/LogIn/LogInForm.js
+++ src/Pages/Authetication/LogIn/LogInForm.js
@@ -5,13 +5,9 @@ const LogInForm = () => {
     return (
         <div class="min-h-screen bg-blue-400 pt-16">
             <div class="bg-white px-16 py-10 mx-auto rounded shadow-2x1 w-1/2">
-                <h2 class="text-3xl font-bold mb-10 text-gray-800">Create Your Account</h2>
+                <h2 class="text-3xl font-bold mb-10 text-gray-800">Log In</h2>
 
                 <form class="space-y-5">
-                    <div>
-                        <label class="block mb-1 font-bold text-gray-500">Name</label>
-                        <input type="text" class="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"/>
-                    </div>
 
                     <div>
                         <label class="block mb-1 font-bold text-gray-500">Email</label>
@@ -23,17 +19,12 @@ const LogInForm = () => {
                         <input type="password" class="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"/>
                     </div>
 
-                    <div class="flex items-center">
-                        <input type="checkbox" id="agree" required/>
-                            <label for="agree" class="ml-2 text-gray-700 text-sm">I agree to the terms and privacy.</label>
-                    </div>
-
                     <div>
-                        <button class="block w-full bg-yellow-400 hover:bg-yellow-300 p-4 rounded text-yellow-900 hover:text-yellow-800 transition duration-300">Sign Up</button>
+                        <button class="block w-full bg-yellow-400 hover:bg-yellow-300 p-4 rounded text-yellow-900 hover:text-yellow-800 transition duration-300">Login</button>
                     </div>
                 </form>
                 
-                <p className='mt-4'>Already registered? <Link to='/user/login' className='text-blue-700 underline'>Sign in</Link></p>
+                <p className='mt-4'>Not a member? <Link to='/user/signup' className='text-blue-700 underline'>Sign up here</Link></p>
             </div>
         </div>
     );
